e163df9c3f038e289ded3644d95b97147402ba5d
P1R47E <yosephtesfaye27@gmail.com>
2025-01-22 10:50:29 +0300

    bleak

--- bleak.py
+++ bleak.py
@@ -1,5 +1,21 @@
 import asyncio
-from bleak import Bleakscanner
+from bleak import BleakScanner
 
 async def scan_for_beacons():
     print("starting BLE device scan...")
+    devices = await BleakScanner.discover()
+    print (f"Found {len(devices)} devices:\n")
+    
+    for device in devices:
+        print(f"Name: {device.name}, Address: {device.address}, RSSI:{device.rrsi}")
+        
+        if "Beacon X Pro" in (device.name or ""):
+            print(">> Detected Beacon X Pro W-6 device!")
+            
+def main():
+    print("Initializing BLE Scanner")
+    asyncio.run(scan_for_beacons())
+    print("scanning completed.")
+if __name__ == "__main__":
+    
+    main()
